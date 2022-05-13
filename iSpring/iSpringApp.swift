7d945009fd7d33eea60f8e3fe979c8d21ba316f4
Your Name <you@example.com>
2022-05-13 12:11:13 -0400

    Initial Commit

---
+++ iSpring/iSpringApp.swift
@@ -0,0 +1,17 @@
+//
+//  iSpringApp.swift
+//  iSpring
+//
+//  Created by Zuhair Shaikh on 2022-05-13.
+//
+
+import SwiftUI
+
+@main
+struct iSpringApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
