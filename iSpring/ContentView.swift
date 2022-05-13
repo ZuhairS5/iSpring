d01f344197e74c2774176ff6c5d58246a770b927
Your Name <you@example.com>
2022-05-13 12:45:39 -0400

    Initial Commit

---
+++ iSpring/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iSpring
+//
+//  Created by Zuhair Shaikh on 2022-05-13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
