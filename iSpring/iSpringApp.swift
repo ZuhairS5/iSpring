28c2c7cbe2c592007174cb4e84328699e4c9fbf9
Samhith Sripada <samhithsripada@yahoo.in>
2022-05-17 13:20:37 -0400

    Integrated Firebase to the project

--- iSpring/iSpringApp.swift
+++ iSpring/iSpringApp.swift
@@ -6,9 +6,16 @@
 //
 
 import SwiftUI
+import Firebase
 
 @main
 struct iSpringApp: App {
+    
+    // required to connect to Firebase
+    init() {
+        FirebaseApp.configure()
+    }
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
